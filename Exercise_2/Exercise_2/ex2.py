752cca955a1021be597644e7928284e0a9089fa2
soizhiwen <zhiwen39@gmail.com>
2023-03-25 21:49:13 +0100

    final ex2

--- Exercise_2/Exercise_2/ex2.py
+++ Exercise_2/Exercise_2/ex2.py
@@ -19,6 +19,7 @@ import utils
 # Cost for node del, node ins, edge del, and edge ins
 TAU = 1.0
 
+
 # You can potentially add more code here (constants, functions, ...)
 
 
@@ -38,27 +39,18 @@ def create_c_mat(g1: nx.Graph, g2: nx.Graph) -> np.ndarray:
         Matrix C
     """
     # Code here
-    n_nodes_g1 = len(g1.nodes())
-    n_nodes_g2 = len(g2.nodes())
-    c_mat = np.zeros((n_nodes_g1 + n_nodes_g2, n_nodes_g1 + n_nodes_g2))
+    n_nodes_g1 = g1.number_of_nodes()
+    n_nodes_g2 = g2.number_of_nodes()
+    total_n_nodes = n_nodes_g1 + n_nodes_g2
+    c_mat = np.zeros((total_n_nodes, total_n_nodes))
 
-    # Compute substitution cost
-    for i in range(n_nodes_g1):
-        for j in range(n_nodes_g2):
-            if g1.nodes[i]["label"] != g2.nodes[j]["label"]:
-                c_mat[i, j] = TAU
+    for i, (u1, attrs1) in enumerate(g1.nodes(data=True)):
+        for j, (u2, attrs2) in enumerate(g2.nodes(data=True)):
+            c_mat[i, j] = np.linalg.norm(attrs1["x"] - attrs2["x"])
+        c_mat[i, n_nodes_g2 + i] = TAU  # deletion of node u1
 
-    # Compute deletion cost
-    for i in range(n_nodes_g1):
-        j = n_nodes_g1 + g2.number_of_nodes()  # placeholder for deleted node
-        for _, _, label in g1.edges(i, data="label"):
-            c_mat[i, j] += TAU
-
-    # Compute insertion cost
-    for j in range(n_nodes_g2):
-        i = n_nodes_g1 + g1.number_of_nodes()  # placeholder for inserted node
-        for _, _, label in g2.edges(j, data="label"):
-            c_mat[i, j] += TAU
+    for j, (u2, attrs2) in enumerate(g2.nodes(data=True)):
+        c_mat[n_nodes_g1 + j, j] = TAU  # insertion of node u2
 
     return c_mat
 
@@ -77,7 +69,18 @@ def augment_c_mat(g1, g2, c_mat) -> np.ndarray:
         Matrix C*
     """
     # Code here
-    return None
+    n_nodes_g1 = g1.number_of_nodes()
+    n_nodes_g2 = g2.number_of_nodes()
+
+    for i, (u1, attrs1) in enumerate(g1.degree):
+        for j, (u2, attrs2) in enumerate(g2.degree):
+            c_mat[i, j] += np.linalg.norm(attrs1 - attrs2)
+        c_mat[i, n_nodes_g2 + i] += attrs1
+
+    for j, (u2, attrs2) in enumerate(g2.degree):
+        c_mat[n_nodes_g1 + j, j] += attrs2
+
+    return c_mat
 
 
 def BP_GED(g1: nx.Graph, g2: nx.Graph) -> float:
@@ -99,7 +102,36 @@ def BP_GED(g1: nx.Graph, g2: nx.Graph) -> float:
         The Graph Edit Distance between g1 and g2
     """
     # Code here
-    return 0.0
+    # 1. Create the C matrix
+    c_mat = create_c_mat(g1, g2)
+
+    # 2. Create the augmented C matrix C*
+    c_star = augment_c_mat(g1, g2, c_mat)
+
+    # 3. Solve the linear sum assignment problem
+    row_ind, col_ind = linear_sum_assignment(c_star)
+
+    # 4. Compute the cost of the node edit operations
+    nodes_cost = 0
+    for i in range(len(row_ind)):
+        if row_ind[i] < g1.number_of_nodes() and col_ind[i] < g2.number_of_nodes():
+            nodes_cost += c_star[row_ind[i], col_ind[i]]
+        elif row_ind[i] < g1.number_of_nodes() and col_ind[i] >= g2.number_of_nodes():
+            nodes_cost += TAU
+        elif row_ind[i] >= g1.number_of_nodes() and col_ind[i] < g2.number_of_nodes():
+            nodes_cost += TAU
+
+    # 5. Compute the cost of the edge edit operations
+    edges_cost = 0
+    if g1.edges() != g2.edges():
+        edges1 = set(g1.edges())
+        edges2 = set(g2.edges())
+        common_edges = edges1.intersection(edges2)
+        edges_cost += len(edges1 - common_edges)
+        edges_cost += len(edges2 - common_edges)
+
+    # 6. Returns the cost of the nodes + cost of the edges
+    return nodes_cost + edges_cost
 
 
 if __name__ == "__main__":
@@ -110,6 +142,12 @@ if __name__ == "__main__":
     utils.draw_all_graphs(graphs, "./drawings", False)
 
     # 2. Compute GED between all pairs of graphs.
+    n = 5
+    D = np.zeros((n, n), dtype=int)
+
+    for i, g1 in enumerate(graphs):
+        for j, g2 in enumerate(graphs):
+            D[i, j] = BP_GED(g1, g2)
 
     # Save the GEDs in './results/GED_results.csv'
-    np.savetxt("./results/GED_results.csv", M, fmt="%i", delimiter=",")
+    np.savetxt("./results/GED_results.csv", D, fmt="%i", delimiter=",")
