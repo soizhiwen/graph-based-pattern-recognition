fd7fdb07ef419533194992e922c37c88ea2e5202
soizhiwen <zhiwen39@gmail.com>
2023-05-12 17:31:22 +0200

    add ex6

---
+++ Exercise_6/Exercise_6/ex6.py
@@ -0,0 +1,17 @@
+import csv
+import os.path
+
+import networkx as nx
+import numpy as np
+
+from utils import load_all_graphs
+from typing import List
+
+
+
+def main():
+    # Code Here
+
+
+if __name__ == '__main__':
+    main()
